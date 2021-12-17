4c3a68f2eb67d3cdd4b299699bf3e4a991723161
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-17 18:54:27 +0700

    feat:channel test:penambahan fitur counter

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -49,5 +50,105 @@ func TestChannelAsParameter(t *testing.T) {
 
 	data := <-channel
 	fmt.Println(data)
+
 	time.Sleep(5 * time.Second)
+}
+
+//cuma boleh mengirim data
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Galih Setiadi"
+
+}
+
+//cuma boleh menerima data
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+//test dari InOutChannel
+func TestInOutChannel(t *testing.T) {
+	//membuat variabel untuk channel
+	channel := make(chan string)
+	//defer untuk memastikan, mau eror/sukse akan dieksekusi
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+//Membuat buffered channel. Buffered channel ibaratkan memory untuk channel. Kalau tidak diberikan buffered,
+//maka channel hanya akan bisa menerima dan mengirim satu data.
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Galih"
+		channel <- "Minavf"
+		channel <- "Setiadi"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("")
+	fmt.Println("Selesai")
+
+}
+
+//Range Channel.Range Channel untuk membuat perulangan, ketika untuk menanggulangi data yang dikirim terus
+//menerus dan kita tidak tahu jelas kapan channel tersebut akan berhenti menerima data
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			//Itoa merupakan konversi dari integer ke string
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		//setiap channel harus di close, dalam kasus ini kalau tidak di close maka perulangan dibawahnya tidak akan pernah berhenti
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+	fmt.Println("Selesai")
+}
+
+//Select channel untuk mendapatkan data dari semua channel
+func TestChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	//pemberian counter untuk menghindari deadlock
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
 }
