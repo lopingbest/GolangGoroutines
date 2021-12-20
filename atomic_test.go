9aa2ee5270948d6e5f97a30d84402bc751394dc6
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-20 14:53:32 +0700

    feat:atomic_test

--- atomic_test.go
+++ atomic_test.go
@@ -1,1 +1,27 @@
 package goroutines
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 1000; i++ {
+		//goroutines anonymous function
+		go func() {
+			group.Add(1)
+			for j := 1; j <= 100; j++ {
+				//memakai atomic
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+	}
+	group.Wait()
+	fmt.Println("Counter =", x)
+}
