33fece2d154d0d6d2a79126ec832ddba1c370c23
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-20 14:01:12 +0700

    feat:channel

--- waitgroup_test.go
+++ waitgroup_test.go
@@ -1,1 +1,30 @@
 package goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	//running sau proses asynchronous
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsynchronous(group)
+	}
+
+	//untuk menggantikan setting waktu (sleep) secara manual
+	group.Wait()
+	fmt.Println("selesai")
+}
