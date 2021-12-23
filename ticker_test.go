176d4b29834fcb564da2cb39a741a9701991b108
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-23 13:25:53 +0700

    featticker_test: add TestTicker, TestTick

--- ticker_test.go
+++ ticker_test.go
@@ -1,1 +1,30 @@
 package goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+//ticker digunakan untuk waktu yang berulang, kalo time.timer digunakan untuk satuwaktu
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	//untuk menghentikan ticker
+	go func() {
+		time.Sleep(5 * time.Second)
+		ticker.Stop()
+	}()
+
+	for time := range ticker.C {
+		fmt.Println(time)
+	}
+}
+
+func TestTick(t *testing.T) {
+	channel := time.Tick(1 * time.Second)
+
+	for time := range channel {
+		fmt.Println(time)
+	}
+}
